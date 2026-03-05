fe0c6ee1a4a769b98cc9264a4dc1228ffc194be8
Reedling-xyz <utkinakateruna5@gmail.com>
2026-03-05 18:02:07 +0300

    часть основы

--- ConsoleA1/Program.cs
+++ ConsoleA1/Program.cs
@@ -1,7 +1,35 @@
-﻿string[] words = {"гроб", "витамин", "чудо", "брюки", "костюм", "середина", "ввод", "услуга", "стадион", "образец", "правило", "замечание", "духи", "бабочка", "буря", "наука", "касса", "движение", "ванная", "целое"};
+﻿using System.Runtime.InteropServices;
+
+string[] words = {"гроб", "витамин", "чудо", "брюки", "костюм", "середина", "ввод", "услуга", "стадион", "образец", "правило", "замечание", "духи", "бабочка", "буря", "наука", "касса", "движение", "ванная", "целое"};
 
 string word = "";
-string guessed = "";
+char[] guessed = {};
+string _guessed = "";
 char guess = ' ';
 
 int lives = 10;
+
+
+if (word != "")
+{
+    if(lives != 0)
+    {
+        for(int i = 0; i < word.Length; i++)
+        {
+            _guessed += guessed[i] + " ";
+        }
+        Console.WriteLine(guessed);
+        Console.WriteLine($"Попыток осталось: {lives}");
+        Console.Write("Угадайте букву: ");
+        guess = Convert.ToChar(Console.Read());
+
+        for(int i = 0; i < word.Length; i++)
+        {
+            if (guess == word[i])
+            {
+                guessed[i] = guess;
+            }
+        }
+    }
+    
+}
