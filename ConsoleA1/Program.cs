3ea58ea7f3df7ff661b0ec3f517ef5dbbe897374
vodolaznica1488 <lera.tyshkovskaya@mail.ru>
2026-03-10 16:34:13 +0300

    start code

--- ConsoleA1/Program.cs
+++ ConsoleA1/Program.cs
@@ -1,7 +1,18 @@
 ﻿string[] words = {"гроб", "витамин", "чудо", "брюки", "костюм", "середина", "ввод", "услуга", "стадион", "образец", "правило", "замечание", "духи", "бабочка", "буря", "наука", "касса", "движение", "ванная", "целое"};
 
 string word = "";
-string guessed = "";
+List <char> guessed = new List <char>();
+string _guessed = "";
 char guess = ' ';
-
 int lives = 10;
+Random rnd = new Random();
+if(word == "")
+{
+    
+    word = words[rnd.Next(words.Length)];
+    for(int i = 0; i < word.Length; i++)
+    {
+        guessed[i] = '_';
+    }
+    
+}
