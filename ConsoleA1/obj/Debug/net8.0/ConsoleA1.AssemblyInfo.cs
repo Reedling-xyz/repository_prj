2b1b190d715d2523fec3381d82b947fdc94cf51d
Reedling-xyz <utkinakateruna5@gmail.com>
2026-03-10 16:46:09 +0300

    ??

--- ConsoleA1/obj/Debug/net8.0/ConsoleA1.AssemblyInfo.cs
+++ ConsoleA1/obj/Debug/net8.0/ConsoleA1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleA1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7844bee4461450dbd4dd8812b52af8aebf3d596")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1bbc2bed990332a66111794b052f570e3ca7e92")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleA1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleA1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
