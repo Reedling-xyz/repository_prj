44f38cf26451a70618694b6a09e7086da7c8f24b
Reedling-xyz <utkinakateruna5@gmail.com>
2026-03-10 17:51:02 +0300

    подкрасивше + цикл + сломанные жизни

--- ConsoleA1/obj/Debug/net8.0/ConsoleA1.AssemblyInfo.cs
+++ ConsoleA1/obj/Debug/net8.0/ConsoleA1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleA1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8ee3851d8d968921371c95c64f2c2ff189816fb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e94b72bd7d194b4e6abe215447df623c9464b5e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleA1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleA1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
